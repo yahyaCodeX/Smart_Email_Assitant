85eb70484a51188c1a098beb71545c60d22dd111
yahya <siddiquiyahya796@gmail.com>
2025-10-27 00:40:52 +0500

    Connected With frontend

--- email-writer-sb/src/main/java/com/email/writer/controller/EmailGeneratorController.java
+++ email-writer-sb/src/main/java/com/email/writer/controller/EmailGeneratorController.java
@@ -4,12 +4,10 @@ import com.email.writer.dtos.EmailRequest;
 import com.email.writer.service.EmailGeneratorService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin("*")
 @RequestMapping("/api/email")
 public class EmailGeneratorController {
 
